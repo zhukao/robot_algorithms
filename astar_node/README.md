2722183ef2e1721f7de6b029cf2088d9aa463289
kao.zhu <kao.zhu@d-robotics.cc>
2025-05-20 15:17:34 +0800

    chore(astar_node): Update vis

--- astar_node/README.md
+++ astar_node/README.md
@@ -6,7 +6,7 @@
 
 2. Generate a map of size `col X row` (default `30 X 10`) and attempt to plan a path.
 
-3. Upon successful planning, visualize the pathfinding results in an image with a resolution of `1920 X 1080`. The rendered information includes obstacles on the map, map boundaries, start point, end point, the planned path, and the search tree.
+3. Upon successful planning, visualize the pathfinding results in an image with a resolution of `1280 X 640`. The rendered information includes obstacles on the map, map boundaries, start point, end point, the planned path, and the search tree.
 
 # Building
 
--- astar_node/src/astar_node.cpp
+++ astar_node/src/astar_node.cpp
@@ -5,14 +5,15 @@
 #include <unordered_set>
 #include <algorithm>
 #include <opencv2/opencv.hpp>
+#include "rclcpp/rclcpp.hpp"
 
-struct Node {
+struct SearchingNode {
     int x, y;
     int g, h; // g: cost from start, h: heuristic to goal
-    bool operator==(const Node& other) const { return x == other.x && y == other.y; }
+    bool operator==(const SearchingNode& other) const { return x == other.x && y == other.y; }
     
     struct Hash {
-        size_t operator()(const Node& n) const {
+        size_t operator()(const SearchingNode& n) const {
             return std::hash<int>()(n.x ^ (n.y << 16));
             // return std::hash<std::string>()(std::to_string(n.x) + std::to_string(n.y));
         }
@@ -24,50 +25,130 @@ struct SearchingProcess {
 
 };
 
-// Heuristic function using Manhattan distance
-int heuristic(const Node& a, const Node& b) {
+class AStarPlannerNode : public rclcpp::Node  {
+public:
+  AStarPlannerNode() : Node("astar_planner") {
+    row = this->declare_parameter("row", row);
+    col = this->declare_parameter("col", col);
+    vis_interval = this->declare_parameter("vis_interval", 10);
+    
+    RCLCPP_WARN(this->get_logger(),
+      "\n vis_interval(step mode is activatied if <=0): %d ms "\
+      "\n row: %d"\
+      "\n col: %d",
+      vis_interval, row, col);
+
+    if (vis_interval <= 0) {
+        step_mode = true;
+        vis_interval = 10;
+        RCLCPP_WARN(rclcpp::get_logger("rrt_node"),
+          "Set vis_interval to %d, step mode enabled. Press SPACE to continue", vis_interval);
+    }
+
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    // 生成随机数作为地图
+    for (int i = 0; i < row; i++) {
+      std::vector<int> temp;
+      for (int j = 0; j < col; j++) {
+        temp.push_back(std::rand() % (std::max(5, std::min(row, col) / 8)));
+      }
+      grid.push_back(temp);
+    }
+    
+    int idx_end_x = grid.size() - 1;
+    int idx_end_y = grid.at(0).size() - 1;
+    // 0 is obstacle, others are free
+    grid[1][0] = 1;
+    grid[idx_end_x][idx_end_y] = 1;
+    
+    start = std::pair<int, int>{1, 0};
+    goal = std::pair<int, int>{idx_end_x, idx_end_y};
+  }
+
+  std::vector<std::pair<int, int>> aStar();
+
+private:
+  int vis_interval = 10;
+  int row = 10;
+  int col = 30;
+  std::pair<int, int> start;
+  std::pair<int, int> goal;
+  std::vector<std::vector<int>> grid;
+  bool step_mode = false;
+
+  int heuristic(const SearchingNode& a, const SearchingNode& b) {
     return abs(a.x - b.x) + abs(a.y - b.y);
-}
+  }
+
+  void WaitAction(bool& planningContinued) {
+    if (!step_mode) {
+      cv::waitKey(vis_interval);
+      return;
+    }
+    planningContinued = false;
+    while (rclcpp::ok()) {
+      int key = cv::waitKey(vis_interval);
+      if (key == 27) {  // ESC键退出
+        rclcpp::shutdown(); 
+      } else if (key == 32) {  // 空格键
+        planningContinued = !planningContinued;
+      } 
+      if (!planningContinued) {
+        continue;
+      } else {
+        break;
+      }
+    }
+  }
+};
+
+// Heuristic function using Manhattan distance
+
 
 // A* algorithm
-std::vector<std::pair<int, int>> aStar(
-    const std::vector<std::vector<int>>& grid,
-    std::pair<int, int> start,
-    std::pair<int, int> goal,
-    SearchingProcess &searching_process
-) {
+std::vector<std::pair<int, int>> AStarPlannerNode::aStar() {
     std::cout << "Run a star, start: " << start.first << ", " << start.second
     << ", goal: " << goal.first << ", " << goal.second
     << "\n";
 
     int rows = grid.size();
     int cols = grid[0].size();
-    std::vector<std::vector<Node>> cameFrom(rows, std::vector<Node>(cols));
+    std::vector<std::vector<SearchingNode>> cameFrom(rows, std::vector<SearchingNode>(cols));
 
-    int cellSize = std::min(1920 / rows, 1080 / cols);
+    int width = 1280;
+    int height = 640;
+    int cellSize = std::min(width / rows, height / cols);
     cv::Mat image(rows * cellSize, cols * cellSize, CV_8UC3, cv::Scalar(255, 255, 255));
 
-    std::priority_queue< Node, std::vector<Node>,
-        std::function<bool(const Node&, const Node&)> > openList(
-      [](const Node& a, const Node& b) {
+    // 创建窗口
+    // 使窗口可调整大小
+    cv::namedWindow("A* Path", cv::WINDOW_NORMAL);
+    // 调整窗口大小
+    cv::resizeWindow("A* Path", width, height);
+    cv::imshow("A* Path", image);
+    cv::waitKey(5);
+
+    std::priority_queue< SearchingNode, std::vector<SearchingNode>,
+        std::function<bool(const SearchingNode&, const SearchingNode&)> > openList(
+      [](const SearchingNode& a, const SearchingNode& b) {
             return (a.g + a.h) > (b.g + b.h);
         });
     
-    std::unordered_set<Node, Node::Hash> closedList;
+    std::unordered_set<SearchingNode, SearchingNode::Hash> closedList;
 
-    Node startNode{start.first, start.second, 0, 0};
+    SearchingNode startNode{start.first, start.second, 0, 0};
     openList.push(startNode);
 
-    {
-      cv::Point center(start.second * cellSize + cellSize / 2,
-        start.first * cellSize + cellSize / 2);
-      cv::circle(image, center, cellSize / 4, cv::Scalar(0, 0, 255), -1); // Path (red)
-    }
-    {
-      cv::Point center(goal.second * cellSize + cellSize / 2,
-        goal.first * cellSize + cellSize / 2);
-      cv::circle(image, center, cellSize / 4, cv::Scalar(0, 0, 255), -1); // Path (red)
-    }
+    // Draw start and goal
+    cv::circle(image, 
+      cv::Point(start.second * cellSize + cellSize / 2,
+      start.first * cellSize + cellSize / 2),
+      cellSize / 4, cv::Scalar(0, 0, 255), -1); // Path (red)
+    cv::circle(image, 
+      cv::Point(goal.second * cellSize + cellSize / 2,
+      goal.first * cellSize + cellSize / 2),
+      cellSize / 4, cv::Scalar(0, 0, 255), -1); // Path (red)
 
     // Draw grid
     for (int x = 0; x < rows; ++x) {
@@ -80,49 +161,48 @@ std::vector<std::pair<int, int>> aStar(
       }
     }
 
-    std::string status = "Press SPACE to start planning, ESC to exit";
-    cv::putText(image, status, cv::Point(cellSize * 0.1, cellSize * 0.5), 
-                cv::FONT_HERSHEY_SIMPLEX, 0.02 * cellSize, cv::Scalar(255, 0, 255), 2);
-
-    while (true) {
-      cv::imshow("A* Path", image);
-      int key = cv::waitKey(10);
-      if (key == 27) {  // ESC键退出
-          return std::vector<std::pair<int, int>>{};
-      } else if (key == 32) {  // 空格键开始规划
-          break;
-      }
-    }
+    std::string status = "Press SPACE to start/pause planning, ESC to exit";
+    cv::putText(image, status, cv::Point(cellSize * 0.1, cellSize * 0.75), 
+                cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 0, 255), 1);
+    cv::imshow("A* Path", image);
 
     // For neighbors exploration
     int dx[] = {-1, 0, 1, 0};
     int dy[] = {0, 1, 0, -1};
     
-    while (!openList.empty()) {
-      Node current = openList.top();
+    while (rclcpp::ok()) {
+      int key = cv::waitKey(vis_interval);
+      if (key == 27) {  // ESC键退出
+        rclcpp::shutdown(); 
+      } else if (key == 32) {  // 空格键
+        break;
+      }
+    }
+
+    bool planningContinued = false;
+    while (rclcpp::ok() && !openList.empty()) {
+      SearchingNode current = openList.top();
       openList.pop();
 
       if (current.x == goal.first && current.y == goal.second) {
         // Reconstruct path
         std::vector<std::pair<int, int>> path;
-        while (current.x != startNode.x || current.y != startNode.y) {
+        while (rclcpp::ok() && current.x != startNode.x || current.y != startNode.y) {
             path.emplace_back(current.x, current.y);
         
             cv::Point center(current.y * cellSize + cellSize / 2,
               current.x * cellSize + cellSize / 2);
-            cv::circle(image, center, cellSize / 4, cv::Scalar(0, 0, 255), -1); // Path (red)
+            cv::circle(image, center, cellSize * 0.2, cv::Scalar(0, 0, 255), -1); // Path (red)
             cv::imshow("A* Path", image);
-            cv::waitKey(20);
-
+            WaitAction(planningContinued);
             current = cameFrom[current.x][current.y];
         }
         path.emplace_back(startNode.x, startNode.y);
         cv::Point center(startNode.y * cellSize + cellSize / 2,
           startNode.x * cellSize + cellSize / 2);
-        cv::circle(image, center, cellSize / 4, cv::Scalar(0, 0, 255), -1); // Path (red)
+        cv::circle(image, center, cellSize * 0.3, cv::Scalar(0, 0, 255), -1); // Path (red)
         cv::imshow("A* Path", image);
-        cv::waitKey(5);
-
+        WaitAction(planningContinued);
         std::reverse(path.begin(), path.end());
         printf("Searching success, path size: %ld\n", path.size());
         return path;
@@ -135,23 +215,22 @@ std::vector<std::pair<int, int>> aStar(
         int ny = current.y + dy[i];
 
         if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && grid[nx][ny] != 0) {
-          Node neighbor{nx, ny, current.g + 1, heuristic({nx, ny}, {goal.first, goal.second})};
+          SearchingNode neighbor{nx, ny, current.g + 1, heuristic({nx, ny}, {goal.first, goal.second})};
           if (closedList.find(neighbor) != closedList.end()) {
             continue;
           } else {
             openList.push(neighbor);
             // printf("open list push (%d, %d)\n", nx, ny);
             cameFrom[nx][ny] = current;
-            searching_process.searched_nodes.push_back({nx, ny});
 
             static int search_idx = 1;
             std::string text = std::to_string(search_idx++);
             cv::putText(image, text,
               cv::Point(ny * cellSize + cellSize * 0.05,
               nx * cellSize + cellSize * 0.75),
-              cv::FONT_HERSHEY_SIMPLEX, 0.02 * cellSize, cv::Scalar(0, 255, 0), 1);
+              cv::FONT_HERSHEY_SIMPLEX, 0.3, cv::Scalar(0, 255, 0), 1);
             cv::imshow("A* Path", image);
-            cv::waitKey(5);
+            WaitAction(planningContinued);
           }
         }
       }
@@ -160,101 +239,25 @@ std::vector<std::pair<int, int>> aStar(
     return {}; // No path found
 }
 
-void visualizePath(const std::vector<std::vector<int>>& grid,
-  const std::vector<std::pair<int, int>>& path,
-  const SearchingProcess& searching_process) {
-    int rows = grid.size();
-    int cols = grid[0].size();
-    int cellSize = std::min(1920 / rows, 1080 / cols);
-    cv::Mat image(rows * cellSize, cols * cellSize, CV_8UC3, cv::Scalar(255, 255, 255));
-
-    // Draw grid
-    for (int x = 0; x < rows; ++x) {
-      for (int y = 0; y < cols; ++y) {
-        cv::Rect rect(y * cellSize, x * cellSize, cellSize, cellSize);
-        if (grid[x][y] == 0)
-            cv::rectangle(image, rect, cv::Scalar(0, 0, 0), -1); // Obstacle (black)
-        else
-            cv::rectangle(image, rect, cv::Scalar(200, 200, 200), 1); // Free space (gray border)
-      }
-    }
-
-    // Draw path
-    for (const auto& p : path) {
-      cv::Point center(p.second * cellSize + cellSize / 2, p.first * cellSize + cellSize / 2);
-      cv::circle(image, center, cellSize / 4, cv::Scalar(0, 0, 255), -1); // Path (red)
-    }
-
-    for (size_t search_idx = 0; search_idx < searching_process.searched_nodes.size(); search_idx++) {
-      std::string text = std::to_string(search_idx + 1);
-      cv::putText(image, text,
-        cv::Point(searching_process.searched_nodes[search_idx].second * cellSize + cellSize * 0.05,
-          searching_process.searched_nodes[search_idx].first * cellSize + cellSize * 0.75),
-        cv::FONT_HERSHEY_SIMPLEX, 0.02 * cellSize, cv::Scalar(0, 255, 0), 1);
-    }
-
-    // Show image
-    cv::imshow("A* Path", image);
-    cv::waitKey(0);
-}
-
 int main(int argc, char ** argv) {
-    std::string usage = "Usage: ./bin [row] [col] [try count]\n";
-    printf("%s\n", usage.data());
+  rclcpp::init(argc, argv);
 
-    int row = 10;
-    int col = 30;
-    int try_count = 50;
-
-    if (argc >= 3)  {
-      row = atoi(argv[1]);
-      col = atoi(argv[2]);
-      if (argc == 4) {
-        try_count = atoi(argv[3]);
-      }
-    } else {
-      // printf("%s\n", usage.data());
-      // return -1;
-    }
-    if (row > 0 && row <= 1000 && col > 0 && col <= 1000 && try_count > 0 && try_count <= 100) {
-      printf("row: %d, col: %d, try_count: %d\n", row, col, try_count);
-    } else {
-      printf("Invalid inputs. row: %d, col: %d, try_count: %d\n", row, col, try_count);
-      return -1;
-    }
+  int try_count = 10;
+  while (rclcpp::ok() && try_count-- > 0) {
+    printf("try_count left: %d\n", try_count);
     
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
-    while (try_count-- > 0) {
-      printf("try_count left: %d\n", try_count);
-      std::vector<std::vector<int>> grid;
-      // 生成随机数作为地图
-      for (int i = 0; i < row; i++) {
-        std::vector<int> temp;
-        for (int j = 0; j < col; j++) {
-          temp.push_back(std::rand() % (std::max(5, std::min(row, col) / 8)));
-        }
-        grid.push_back(temp);
+    AStarPlannerNode astar_node;
+if (!astar_node.aStar().empty()) {
+      while (rclcpp::ok()) {
+        cv::waitKey(10);
       }
-
-      int idx_end_x = grid.size() - 1;
-      int idx_end_y = grid.at(0).size() - 1;
-      
-      // 0 is obstacle, others are free
-      grid[0][0] = 1;
-      grid[idx_end_x][idx_end_y] = 1;
-
-      SearchingProcess searching_process;
-      auto path = aStar(grid, {0, 0}, {idx_end_x, idx_end_y},
-        searching_process);
-      if (!path.empty()) {
-        cv::waitKey(0);
-        return 0;
-      } else {
-        std::cout << "No path found.\n";
-        continue;
-      }
+      return 0;
+    } else {
+      std::cout << "No path found.\n";
+      continue;
     }
+  }
 
-    return 0;
+  rclcpp::shutdown();
+  return 0;
 }
